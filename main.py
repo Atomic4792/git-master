ddbd465e3a1926c4f245ba2fcbe063e19e44b98f
Oscar <oscar5tob@gmail.com>
2026-02-13 10:15:30 -0500

    add random data from api

--- main.py
+++ main.py
@@ -1,1 +1,16 @@
-print("hello")
+from dotenv import load_dotenv
+import os
+import requests
+
+load_dotenv()  # loads .env into environment variables
+
+url = "https://api.json-generator.com/templates/2ireVV0w37TX/data"
+token = os.getenv("JSON_API_TOKEN")
+print(token)
+
+headers = {"Authorization": f"Bearer {token}"}
+print("HEADER SENT:", headers)
+
+r = requests.get(url, headers=headers)
+print(r.status_code)
+print(r.json())
