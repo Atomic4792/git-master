3d0a609208494b55992b73dc767a6b5b98bf97a7
Eng. Torrealba <otorrealba019@gmail.com>
2026-02-18 23:13:58 -0500

    Display 4 columns instead of full table

--- view.py
+++ view.py
@@ -12,4 +12,5 @@ def print_table(json_data: list) -> DataFrame:
     #read_json and StringIO work together to create a DataFrame object
     #without too much hassle. Without them it would take a couple loops
     #and new funtions to achieve the same thing. READ THE DOCS
-    return read_json(StringIO(dumps(json_data)))
+    json_table_dataframe= read_json(StringIO(dumps(json_data)))
+    return json_table_dataframe[['id','email', 'username','roles']]
