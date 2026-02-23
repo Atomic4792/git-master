41467bc4620c1d4a8ed0a5190271cd94b6712fe0
Eng. Torrealba <otorrealba019@gmail.com>
2026-02-23 15:51:23 -0500

    clean up: delete unused imported function in view and commented out block of code

--- view.py
+++ view.py
@@ -1,11 +1,6 @@
 from json import dumps
 from pandas import DataFrame, read_json
-from io import StringIO
-from error_handler import int_prompt_validator
-
-# def get_row_num() -> int:
-#     row_num = int(input("How many rows would you like to see?: "))
-#     return row_num
+from io import StringIO 
 
 
 def print_table(json_data: list) -> DataFrame:
